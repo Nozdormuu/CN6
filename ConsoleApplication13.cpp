eefd9ac58ecdcfe306f9ced10e453a56e1ae5534
Nozdormuu <113297594+Nozdormuu@users.noreply.github.com>
2024-11-03 20:25:08 +0300

    Add files via upload

---
+++ ConsoleApplication13.cpp
@@ -0,0 +1,73 @@
+﻿#undef UNICODE
+#pragma warning(disable : 4996)
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <winsock2.h>
+
+#define SERVER_IP "127.0.0.1"
+#define PORT 27015
+#define BUFFER_SIZE 1024
+
+#pragma comment (lib, "Ws2_32.lib")
+#pragma comment (lib, "Mswsock.lib")
+#pragma comment (lib, "AdvApi32.lib")
+
+int main() {
+   int sock;
+   struct sockaddr_in serverAddr;
+   char buffer[BUFFER_SIZE];
+
+   
+   WSADATA wsaData;
+   if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+      printf("WSAStartup failed: %dn", WSAGetLastError());
+      return 1;
+   }
+
+   
+   sock = socket(AF_INET, SOCK_DGRAM, 0);
+   if (sock == INVALID_SOCKET) {
+      printf("Socket creation failed: %dn", WSAGetLastError());
+      WSACleanup();
+      return 1;
+   }
+
+   
+   serverAddr.sin_family = AF_INET;
+   serverAddr.sin_addr.s_addr = inet_addr(SERVER_IP);
+   serverAddr.sin_port = htons(PORT);
+
+   while (true) {
+   
+      printf("Enter message: ");
+      fgets(buffer, BUFFER_SIZE, stdin);
+
+      
+      buffer[strcspn(buffer, "\n")] = '\0';
+
+      
+      int sendResult = sendto(sock, buffer, strlen(buffer), 0, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
+      if (sendResult == SOCKET_ERROR) {
+         printf("Send failed: %dn", WSAGetLastError());
+         break; 
+      }
+
+     
+      int addrLen = sizeof(serverAddr);
+      int bytesReceived = recvfrom(sock, buffer, BUFFER_SIZE - 1, 0, (struct sockaddr*)&serverAddr, &addrLen);
+      if (bytesReceived == SOCKET_ERROR) {
+         printf("Receive failed: %dn", WSAGetLastError());
+         break; 
+      }
+
+      buffer[bytesReceived] = '\0'; 
+      printf("Received from server: %s\n", buffer);
+   }
+
+   closesocket(sock);
+   WSACleanup();
+
+   return 0;
+}
---
+++ ConsoleApplication14.cpp
@@ -0,0 +1,51 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <winsock2.h>
+
+#pragma comment (lib, "Ws2_32.lib")
+#define PORT 27015
+#define BUFFER_SIZE 1024
+
+int main() {
+   int sock;
+   struct sockaddr_in serverAddr, clientAddr;
+   int clientAddrLen = sizeof(clientAddr);
+   char buffer[BUFFER_SIZE];
+
+   
+   WSADATA wsaData;
+   WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+   
+   sock = socket(AF_INET, SOCK_DGRAM, 0);
+
+   
+   serverAddr.sin_family = AF_INET;
+   serverAddr.sin_addr.s_addr = INADDR_ANY;
+   serverAddr.sin_port = htons(PORT);
+
+   
+   bind(sock, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
+
+   printf("UDP Server is running on port %d...\n", PORT);
+
+   while (true) {
+      
+      int bytesReceived = recvfrom(sock, buffer, BUFFER_SIZE, 0, (struct sockaddr*)&clientAddr, &clientAddrLen);
+      buffer[bytesReceived] = '\0';
+      printf("Received: %s\n", buffer);
+
+         
+         sendto(sock, buffer, bytesReceived, 0, (struct sockaddr*)&clientAddr, clientAddrLen);
+
+      
+      printf("Message sent back to client: %s\n", buffer);
+   }
+
+   closesocket(sock);
+   WSACleanup();
+
+   return 0;
+}
